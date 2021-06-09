8a46d93478a016ccc64a829bf6be6aae095daf10
zachtyll <zacharias.tyllstrom@gmail.com>
2021-06-09 15:11:16 +0200

    Initial commit.

---
+++ Control.gd
@@ -0,0 +1,229 @@
+extends Control
+
+const matrix_protocol = preload( "Matrix.gd" )
+
+var access_token = ""
+var login : bool = false
+var input_text := ""
+var mp : MatrixProtocol
+var room_counter := 0
+var joined_rooms := []
+var current_room = ""
+var next_batch := ""
+
+onready var channel_name := $Screen/MidSection/TopBarMid/TopBarMidHbox/ChannelLabels/ChannelName
+onready var topic := $Screen/MidSection/TopBarMid/TopBarMidHbox/ChannelLabels/Topic
+onready var chat_history_list := $Screen/MidSection/ItemList
+onready var room_list := $Screen/LeftSection/ItemList
+onready var chat_line := $Screen/MidSection/ChatInput/LineEdit
+
+
+# Sends a message to the given room
+func _on_Button2_pressed() -> void:
+	if input_text.empty():
+		return
+	_send_message()
+
+
+func _send_message() -> void:
+	if chat_line.text.empty():
+		return
+	elif chat_line.text.begins_with(" "):
+		return
+	mp.send_message(current_room, input_text)
+	chat_line.clear()
+	# We need to yield here, so that we catch the newly sent message.
+	yield(mp, "send_message_completed")
+	mp.get_messages(current_room, next_batch, "", "b", 1, "")
+
+
+# Allows the user to join a room.
+func _on_Button3_pressed():
+	mp.join_room(input_text)
+
+
+# Register a new user.
+# TODO : Figure out how to register a user
+# TODO : Implement registration.
+func _on_Button4_pressed():
+	mp.register()
+
+
+# Sync all events. Should only be used on startup.
+# TODO : Use only sync on startup.
+func _on_Button5_pressed():
+#	var url := 'https://matrix.org/_matrix/client/r0/sync?filter={"room":{"timeline":{"limit":10}}}&since=s9_13_0_1_1&access_token=' + str(access_token)
+	mp.sync_events("filter={'room':{'timeline':{'limit':10}}}", next_batch)
+
+
+# Legacy content getter.
+# TODO : Make this sync only on startup and filter less data.
+func update_list(response) -> void:
+	if not response.has("next_batch"):
+		return
+	# Set next message batch to be expected
+	next_batch = response.get("next_batch")
+	# Ugly algo to get message text only
+	var rooms = response["rooms"].get("join").keys()
+	var event_array = []
+	for key in rooms:
+		# TODO : Refactor this algo.
+		event_array.append(response["rooms"].get("join").get(key).get("timeline").get("events"))
+		var something = response["rooms"].get("join").get(key).get("timeline").get("events")
+		for i in something.size():
+			if something[i].get("content").get("body") is String:
+				chat_history_list.add_item(something[i].get("content").get("body"))
+
+
+# Add rooms to our room list in the left navbar
+# TODO : Currently broken due to "Unrecognized request" in "room_aliases".
+func _update_room_list(room_name : Dictionary) -> void:
+	if not room_name.has("name"):
+		return	
+	room_list.add_item(room_name.get("name"))
+
+
+# Translates room id into a human-readable alias.
+# TODO : Make it work the way it should according to spec.
+# TODO : Make this work with room_alias_by_id instead
+# 	of using the hacky way of finding canonical alias.
+func _translate_room_id(rooms : Dictionary):
+	for room_id in rooms["joined_rooms"]:
+		joined_rooms.append(room_id)
+		mp.get_room_name_by_room_id(room_id)
+
+
+
+# OS notification when we recieve a message and not in focus on screen
+func _notify_user() -> void:
+	OS.request_attention()
+
+
+# Triggers when a login call has completed.
+func _on_login_completed(success):
+	if success:
+		mp.get_joined_rooms()
+		mp.sync_events('filter={"room":{"timeline":{"limit":10}}}', next_batch)
+	elif not success:
+		print("Invalid login!")
+	else:
+		push_error("Unknown login error!")
+
+
+# Updates the input text from the LineEdit in chat section
+func _on_LineEdit_text_changed(new_text):
+	input_text = new_text
+
+
+# Login and logout
+func _on_Button_toggled(button_pressed):
+	if button_pressed:
+		mp.login("zactyl", "GeMigMatrixNu!")
+#			"user": "zactyl"
+#			"user": "kungpost"
+#			"password": "GeMigMatrixNu!",
+#			"password": "kungpost123",
+	else:
+		mp.logout()
+		room_list.clear()
+		room_counter = 0
+		chat_history_list.clear()
+
+
+# Updates which room we act upon via the left sidebar
+# TODO : Fix so that we can see old and new messages at the same time.
+func _on_room_list_item_selected(index):
+	chat_history_list.clear()
+	current_room = joined_rooms[index]
+	channel_name.text = room_list.get_item_text(index)
+#	room_id : String,
+#	from : String = next_batch,
+#	to : String = "",
+#	dir : String = "b",
+#	limit : int = 10,
+#	filter : String = ""
+#	mp.get_messages(current_room, next_batch, "", "b", 10, "")
+	mp.get_messages(current_room, next_batch, "", "b", 10, "")
+
+
+# Updates the chat window when we click on a room in the left sidebar
+func _update_chat_window(messages : Dictionary) -> void:
+	if messages.has("chunk"):
+		messages.get("chunk").invert()
+		for content in messages.get("chunk"):
+			chat_history_list.add_item(_format_chat(content))
+
+
+func _format_chat(content) -> String:
+	var message_line := ""
+	if not content == null:
+		match(content.get("type")):
+			"m.room.message":
+				match(content.get("content").get("msgtype")):
+					"m.text":
+						message_line += content.get("sender") + ": "
+						message_line += content.get("content").get("body")
+					_:
+						print("Can only handle text right now!")
+						print("This is a: " + str(content.get("msgtype")))
+			"m.room.guest_access":
+				print("TODO : Implement m.room.guest_access")
+				print(content.get("content"))
+			"m.room.member":
+				print("TODO : Implement m.room.member")
+				print(content.get("content"))
+				print(content.get("content").get("room_alias_name") is String)
+				
+				if content.get("content").has("room_alias_name"):
+					channel_name.text = content.get("content").get("room_alias_name")
+			"m.room.create":
+				print("TODO : Implement m.room.create")
+				print(content.get("content"))
+			"m.room.history_visibility":
+				print("TODO : Implement m.room.history_visibility")
+				print(content.get("content"))
+			"m.room.join_rules":
+				print("TODO : Implement m.room.join_rules")
+				print(content.get("content"))
+			"m.room.canonical_alias":
+				print("TODO : Implement m.room.canonical_alias")
+				print(content.get("content"))
+			"m.room.topic":
+				print("TODO : Implement m.room.topic")
+#				topic.text = content.get("content").get("topic")
+			"m.room.name":
+				print("TODO : Implement m.room.name")
+				print(content.get("content"))
+			"m.room.power_levels":
+#				print("TODO : Implement m.room.power_levels")
+				print(content.get("content"))
+			"m.room.encrypted":
+#				print("TODO : Implement m.room.encrypted")
+				pass
+			"m.reaction":
+				pass
+			"m.room.redaction":
+#				print(content.get("content"))
+				pass
+			_:
+				push_warning("Unhandled message in content block: " + str(content.get("type")))
+	return message_line
+
+
+func _input(event):
+	if event.is_action_pressed("Enter"):
+		_send_message()
+
+
+func _ready():
+	mp = matrix_protocol.new() as MatrixProtocol
+	self.add_child(mp)
+	var _sync_err = mp.connect("sync_completed", self, "update_list")
+#	var _room_create_err = mp.connect("create_room_completed", self, "update_chat_list")
+	var _login_err = mp.connect("login_completed", self, "_on_login_completed")
+	var _get_joined_rooms_err = mp.connect("get_joined_rooms_completed", self, "_translate_room_id")
+#	var _get_room_id_by_alias_err = mp.connect("get_room_id_by_alias_completed", self, "_update_room_list")
+#	var _get_room_aliases_err = mp.connect("get_room_aliases_completed", self, "_update_room_list")
+	var _get_messages_err = mp.connect("get_messages_completed", self, "_update_chat_window")
+#	var _get_state_by_room_id_err = mp.connect("get_state_by_room_id_completed", self, "_update_room_list")
+	var _get_name_by_room_id_err = mp.connect("get_room_name_by_room_id_completed", self, "_update_room_list")
---
+++ Matrix.gd
@@ -0,0 +1,411 @@
+class_name MatrixProtocol
+extends Node
+
+enum Preset {
+	PRIVATE_CHAT,
+	PUBLIC_CHAT,
+	TRUSTED_PRIVATE_CHAT
+}
+
+var access_token := ""
+#var next_batch := "s9_13_0_1_1"
+
+# Signals since it's async calls.
+signal sync_completed
+signal login_completed
+signal logout_completed
+signal room_joined_completed
+signal send_message_completed
+signal get_members_completed
+signal create_room_completed
+signal get_joined_rooms_completed
+signal get_room_id_by_alias_completed
+signal get_room_aliases_completed
+signal get_messages_completed
+signal get_state_by_room_id_completed
+signal get_room_name_by_room_id_completed
+
+
+# Logs a user into Matrix
+# TODO : Make this work with any homeserver
+func login(username : String, password : String):
+	var device_id := OS.get_unique_id() + "_" + username
+	var url := "https://matrix.org/_matrix/client/r0/login"
+	var body := {
+		"identifier": {
+		"type": "m.id.user",
+		"user": username
+		},
+		"initial_device_display_name": device_id,
+		"password": password,
+		"type": "m.login.password"
+		}
+	_make_post_request(url, body, true, "_login_completed")
+
+
+# Logs out a session from the server
+func logout():
+	var url := "https://matrix.org/_matrix/client/r0/logout?access_token=" + access_token
+	var body := {}
+	
+	_make_post_request(url, body, true, "_logout_completed")
+
+
+# Registers a new user on Matrix.
+# TODO : Make this work as intended.
+# TODO : Allow for 3PID registration.
+func register():
+	print("Not implemented.")
+
+# Sends a message to the server.
+# NOTE : Message does not always mean "text message".
+# NOTE : Returns a message ID for redactions and edits.
+func send_message(room : String, message : String):
+	var url := "https://matrix.org/_matrix/client/r0/rooms/" + room + "/send/m.room.message?access_token=" + access_token
+	var content := {
+		"msgtype" : "m.text",
+		"body" : message
+		}
+	_make_post_request(url, content, true, "_send_message_completed")
+
+
+# Syncs client state with server state.
+# NOTE : Should be used only on startup.
+# NOTE : Heavy workload.
+func sync_events(filter : String, next_batch : String):
+	var url := "https://matrix.org/_matrix/client/r0/sync?" + filter + "&" + "since=" + next_batch + "&access_token=" + access_token
+	print(url)
+	_make_get_request(url, "_sync_completed")
+
+
+# Joins the user to a room.
+func join_room(room : String):
+	var url := "https://matrix.org/_matrix/client/r0/join/" + room + "?access_token=" + access_token
+	var body := {
+		"room_alias_name": "test"
+	}
+	_make_post_request(url, body, true, "_join_room_completed")
+
+
+# Gets all members of a room.
+func get_members(room : String):
+	var url := "https://matrix.org/_matrix/client/r0/rooms/" + room + "/joined_members?access_token=" + access_token
+	_make_get_request(url, "_get_members_completed")
+
+
+# Creates a new room.
+func create_room(room_name : String, preset : int, room_alias_name : String, topic : String):
+	var url := "https://matrix.org/_matrix/client/r0/createRoom?access_token=" + access_token
+	var room_preset := ""
+	
+	match(preset):
+		Preset.PRIVATE_CHAT:
+			room_preset = "private_chat"
+		Preset.PUBLIC_CHAT:
+			room_preset = "public_chat"
+		Preset.TRUSTED_PRIVATE_CHAT:
+			room_preset = "trusted_private_chat"
+			
+	var body := {
+		"creation_content": {
+		"m.federate": false
+		},
+		"name": room_name,
+		"preset": room_preset,
+		"room_alias_name": room_alias_name,
+		"topic": topic
+	}
+	_make_post_request(url, body, true, "_create_room_completed")
+
+
+# Gets all rooms the user has joined.
+func get_joined_rooms():
+	var url := "https://matrix.org/_matrix/client/r0/joined_rooms?access_token=" + access_token
+	_make_get_request(url, "_get_joined_rooms_completed")
+
+
+# Gets a room id by reading an alias.
+func get_room_id_by_alias(alias : String, homeserver : String = "matrix.org") -> void:
+	var url := "https://matrix.org/_matrix/client/r0/directory/room/" + alias + homeserver + "?access_token=" + access_token
+	_make_get_request(url, "_get_room_id_by_alias_completed")
+
+
+# Gets an array of local aliases for a room.
+# TODO : Figure out what "local" implies.
+func get_room_aliases(room_id : String) -> void:
+	var url := "https://matrix.org/_matrix/client/r0/rooms/" + room_id + "/aliases?access_token=" + access_token
+	_make_get_request(url, "_get_room_aliases_completed")
+
+
+# Gets messages from room "room_id". Batch "from" to batch "to" with max "limit" messages.
+# "dir" handles the direction to paginate. ("b"ackwards or "f"orwards).
+# TODO : Figure out how "filter" works.
+func get_messages(room_id : String, from : String, to : String = "", dir : String = "b", limit : int = 10, filter : String = ""):
+	var url := "https://matrix.org/_matrix/client/r0/rooms/" + room_id + "/messages?from=" + from + "&to=" + to + "&dir=" + dir + "&limit=" + str(limit) +  "&filter=" + filter + "&access_token=" + access_token
+	_make_get_request(url, "_get_messages_completed")
+
+
+func get_state_by_room_id(room_id : String) -> void:
+	var url := "https://matrix.org/_matrix/client/r0/rooms/" + room_id + "/state?access_token=" + access_token
+	_make_get_request(url, "_get_state_by_room_id_completed")
+
+
+func get_room_name_by_room_id(room_id : String) -> void:
+	var url := "https://matrix.org/_matrix/client/r0/rooms/" + room_id + "/state/m.room.name/?access_token=" + access_token
+	_make_get_request(url, "_get_room_name_by_room_id_completed")
+
+# Called when the login request is completed.
+# Sets the access token that is used for all interactions with the server.
+# NOTE : Prints "response" for debugging via API playground at matrix.org
+func _login_completed(result : int, response_code : int, _headers : PoolStringArray, body : PoolByteArray):
+	var _err_result = _check_result(result)
+	var response: Dictionary = parse_json(body.get_string_from_ascii())
+	match(response_code):
+		400:
+			print("Part of the request was invalid. For example, the login type may not be recognised.")
+			emit_signal("login_completed", false)
+		403:
+			print("The login attempt failed. This can include one of the following error codes:")
+			emit_signal("login_completed", false)
+		429:
+			print("This request was rate-limited.")
+			emit_signal("login_completed", false)
+		200:
+			print("Success!")
+			# NOTE : access_token must be set before emitting signal.
+			access_token = response.get("access_token")
+			emit_signal("login_completed", true)
+			print(response)
+		_:
+			print("something unexpected happened: " + str(response_code))
+	
+
+# Runs when the logout request completes.
+# Requires an access token to know who is logging out.
+func _logout_completed(result : int, response_code : int, _headers : PoolStringArray, _body : PoolByteArray):
+	var _err_result = _check_result(result)
+	match(response_code):
+		200:
+			access_token = ""
+			print("Successfully logged out")
+			emit_signal("logout_completed", true)
+		401:
+			print("Missing access token")
+			emit_signal("logout_completed", false)
+
+
+# Runs when synchronizaion completes.
+# Sets the "next_batch" parameter that is used to paginate the server.
+func _sync_completed(result : int, response_code : int, _headers : PoolStringArray, body : PoolByteArray):
+	var _err_result = _check_result(result)
+	var response: Dictionary = parse_json(body.get_string_from_ascii())
+	match(response_code):
+		200:
+			print("Sync success")
+			# Set next message batch to be expected
+#			next_batch = response.get("next_batch")
+			# Ugly algo to get message text only
+		401:
+			print("Missing access token")
+	emit_signal("sync_completed", response)
+
+
+# Runs when send message has completed.
+func _send_message_completed(result : int, _response_code : int, _headers : PoolStringArray, _body : PoolByteArray):
+	var _err_result = _check_result(result)
+	emit_signal("send_message_completed", true)
+
+
+# Runs when join room has completed.
+# TODO : Remove unessecary return statement.
+# TODO : Figure out if we actually need this signal to be emitted.
+func _join_room_completed(result : int, response_code : int, _headers : PoolStringArray, body : PoolByteArray) -> String:
+	var _err_result = _check_result(result)
+	var response: Dictionary = parse_json(body.get_string_from_ascii())
+	match(response_code):
+		200:
+			print("Join room success")
+			emit_signal("room_joined_completed", true)
+		403:
+			print("An unkown error occurred")
+			emit_signal("room_joined_completed", false)
+		429:
+			print("Too many requests")
+			emit_signal("room_joined_completed", false)
+	return response.get("room_id")
+
+
+# Runs when get members completes
+func _get_members_completed(result : int, response_code : int, _headers : PoolStringArray, body : PoolByteArray) -> void:
+	var _err_result = _check_result(result)
+	var response: Dictionary = parse_json(body.get_string_from_ascii())
+	match(response_code):
+		200:
+			print("Get members success")
+		403:
+			print("You are not a member of the room")
+	emit_signal("get_members_completed", response)
+
+
+# Runs when create room completes.
+func _create_room_completed(result : int, response_code : int, _headers : PoolStringArray, body : PoolByteArray) -> void:
+	var _err_result = _check_result(result)
+	var response: Dictionary = parse_json(body.get_string_from_ascii())
+	match(response_code):
+		200:
+			print("Room create success")
+		400:
+			print("Unknown error occurred")
+	emit_signal("create_room_completed", response)
+
+
+# Runs when get joined rooms completes.
+func _get_joined_rooms_completed(result : int, _response_code : int, _headers : PoolStringArray, body : PoolByteArray) -> void:
+	var _err_result = _check_result(result)
+	var response: Dictionary = parse_json(body.get_string_from_ascii())
+	emit_signal("get_joined_rooms_completed", response)
+
+
+# Runs when get room by alias completes.
+func _get_room_id_by_alias_completed(result : int, _response_code : int, _headers : PoolStringArray, body : PoolByteArray) -> void:
+	var _err_result = _check_result(result)
+	var response: Dictionary = parse_json(body.get_string_from_ascii())
+	emit_signal("get_room_id_by_alias_completed", response)
+
+
+# Runs when get room aliases completes.
+func _get_room_aliases_completed(result : int, _response_code : int, _headers : PoolStringArray, body : PoolByteArray) -> void:
+	var _err_result = _check_result(result)
+	var response: Dictionary = parse_json(body.get_string_from_ascii())
+	emit_signal("get_room_aliases_completed", response)
+
+
+# Runs when get messages completes.
+func _get_messages_completed(result : int, response_code : int, _headers : PoolStringArray, body : PoolByteArray) -> void:
+	var _err_result = _check_result(result)
+	var response: Dictionary = parse_json(body.get_string_from_ascii())
+	match(response_code):
+		200:
+			print("Get messages success")
+		403:
+			print("You are not a member of this room")
+	emit_signal("get_messages_completed", response)
+
+
+func _get_state_by_room_id_completed(result : int, response_code : int, _headers : PoolStringArray, body : PoolByteArray) -> void:
+	var _err_result = _check_result(result)
+	var response: Array = parse_json(body.get_string_from_ascii())
+	match(response_code):
+		200:
+			print("Get state success")
+		403:
+			print("You aren't a member of the room and weren't previously a member of the room.")
+	
+	emit_signal("get_state_by_room_id_completed", response)
+
+
+func _get_room_name_by_room_id_completed(result : int, response_code : int, _headers : PoolStringArray, body : PoolByteArray) -> void:
+	var _err_result = _check_result(result)
+	var response: Dictionary = parse_json(body.get_string_from_ascii())
+	match(response_code):
+		200:
+			print("Get messages success")
+		404:
+			print("The room has no state with the given type or key.")
+		403:
+			print("You aren't a member of the room and weren't previously a member of the room.")
+	emit_signal("get_room_name_by_room_id_completed", response)
+	
+
+
+# HTTPRequest POST mode.
+# Connects a callback to functions in class for assync behaviour.
+func _make_post_request(url : String, data_to_send, use_ssl : bool, response : String):
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.connect("request_completed", self, response)
+	# Convert data to json string:
+	var query = JSON.print(data_to_send)
+	# Add 'Content-Type' header:
+	var headers = ["Content-Type: application/json"]
+	var error = http_request.request(url, headers, use_ssl, HTTPClient.METHOD_POST, query)
+	
+	if error != OK:
+		push_error("An error occurred in the HTTP request.")
+	
+
+# HTTPRequest PUT mode.
+# Connects a callback to functions in class for assync behaviour.
+func _make_put_request(url : String, data_to_send, use_ssl : bool, response : String):
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.connect("request_completed", self, response)
+	# Convert data to json string:
+	var query = JSON.print(data_to_send)
+	# Add 'Content-Type' header:
+	var headers = ["Content-Type: application/json"]
+	var error = http_request.request(url, headers, use_ssl, HTTPClient.METHOD_PUT, query)
+	
+	if error != OK:
+		push_error("An error occurred in the HTTP request.")
+
+
+# HTTPRequest GET mode.
+# Connects a callback to functions in class for assync behaviour.
+func _make_get_request(url : String, response : String):
+	# Create an HTTP request node and connect its completion signal.
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.connect("request_completed", self, response)
+	# Perform a GET request. The URL below returns JSON as of writing.
+	var error = http_request.request(url)
+	if error != OK:
+		push_error("An error occurred in the HTTP request.")
+
+
+# Checks if the HTTP request response is broken somehow.
+func _check_result(result) -> bool:
+	match(result):
+		HTTPRequest.RESULT_SUCCESS:
+			print("Request successful.")
+			return true
+		HTTPRequest.RESULT_CHUNKED_BODY_SIZE_MISMATCH:
+			push_error("Result chunked body size mismatch.")
+			return false
+		HTTPRequest.RESULT_CANT_CONNECT:
+			push_error("Request failed while connecting.")
+			return false
+		HTTPRequest.RESULT_CANT_RESOLVE:
+			push_error("Request failed while resolving.")
+			return false
+		HTTPRequest.RESULT_CONNECTION_ERROR:
+			push_error("Request failed due to connection (read/write) error.")
+			return false
+		HTTPRequest.RESULT_SSL_HANDSHAKE_ERROR:
+			push_error("Request failed on SSL handshake.")
+			return false
+		HTTPRequest.RESULT_NO_RESPONSE:
+			push_error("Request does not have a response (yet).")
+			return false
+		HTTPRequest.RESULT_BODY_SIZE_LIMIT_EXCEEDED:
+			push_error("Request exceeded its maximum size limit, see body_size_limit.")
+			return false
+		HTTPRequest.RESULT_REQUEST_FAILED:
+			push_error("Request failed (currently unused).")
+			return false
+		HTTPRequest.RESULT_DOWNLOAD_FILE_CANT_OPEN:
+			push_error("HTTPRequest couldn’t open the download file.")
+			return false
+		HTTPRequest.RESULT_DOWNLOAD_FILE_WRITE_ERROR:
+			push_error("HTTPRequest couldn’t write to the download file.")
+			return false
+		HTTPRequest.RESULT_REDIRECT_LIMIT_REACHED:
+			push_error("Request reached its maximum redirect limit, see max_redirects.")
+			return false
+		HTTPRequest.RESULT_TIMEOUT:
+			push_error("Timeout!")
+			return false
+		_:
+			push_error("Unexpected error!")
+			return false
+
