3bcb4411266d3514f88866cb3209188cda450d58
zachtyll <zacharias.tyllstrom@gmail.com>
2021-06-24 19:36:13 +0200

    Room naming algorithm now seems to name rooms correctly at any time.

--- CustomClasses/Room.gd
+++ CustomClasses/Room.gd
@@ -3,13 +3,12 @@ extends Node
 # Model for a Matrix room.
 
 
-
+# Container for all recorded events in a room.
 class Timeline:
-# Container for all events in a room.
-	var events = []
+	var events := []
 	
 	
-	func _init(new_timeline):
+	func _init(new_timeline : Dictionary):
 		for event in new_timeline["events"]:
 			var new_event = Event.new(event)
 			event_append(new_event)
@@ -50,15 +49,38 @@ class Timeline:
 		return events[index]["event_id"]
 
 
+# Container for all recorded state updates of a room
+class State:
+	var events := []
+
+
+	func _init(new_state : Dictionary):
+		for event in new_state["events"]:
+			var new_event = Event.new(event)
+			event_append(new_event)
+
+
+	func event_append(event : Event) -> void:
+		if event is Event:
+			events.append(event)
+		else:
+			push_error("Timeline rejects: %s" % event)
+
+
+	func get_event(index : int) -> Event:
+		return events[index]
+
+
 var room_id := ""
 var room_name := ""
 var room_alias := ""
 var room_topic := ""
 var room_membership := 0
 
-# Room keys
+# Events that are availiable from sync.
 var timeline : Timeline = null
-var state
+# Room state events not included in timeline.
+var state : State = null
 var account_data
 var ephemeral
 var unread_notifications
@@ -68,7 +90,7 @@ var org_matrix_msc2654_unread_count
 
 func _init(username : String, new_room_id : String, new_room_data : Dictionary):
 	timeline = Timeline.new(new_room_data["timeline"])
-	state = new_room_data["state"]
+	state = State.new(new_room_data["state"])
 	room_id = new_room_id
 	_get_room_name(username)
 	_get_room_topic()
@@ -77,16 +99,22 @@ func _init(username : String, new_room_id : String, new_room_data : Dictionary):
 # Massive function to translate room_id into human-readable names.
 # TODO : Move this algo into the Room class.
 func _get_room_name(username : String) -> void:
+	print(room_id)
 	var room_member_name := ""
-	for event in timeline.events:
+	# Must check state.events for rooms with a lot of message events.
+	for event in (timeline.events + state.events):
 		match(event.type):
 			"m.room.name":
 				room_name = event.content["name"]
 			"m.room.canonical_alias":
 				room_alias = event.content["alias"]
 			"m.room.member":
-				if not event.content["displayname"] == username:
-					room_member_name = event.content["displayname"]
+				if not event.content["membership"] == "leave":
+					if event.content.has("room_alias_name"):
+						room_member_name = event.content["room_alias_name"]
+					elif not event.content["displayname"] == username:
+						room_member_name = event.content["displayname"]
+	print("- - - - END - - - -")
 	if not room_name.empty():
 		return
 	elif not room_alias.empty():
@@ -95,6 +123,7 @@ func _get_room_name(username : String) -> void:
 		room_name = room_member_name
 	else:
 		room_name = str(self)
+		print(timeline.events)
 
 
 func _get_room_topic():
